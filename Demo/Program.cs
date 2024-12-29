fad643034fe4be3d9e081cfd52aeb059c40fd338
Mohamed Shaban <mohamedshabban707@gmail.com>
2024-12-29 15:47:26 +0200

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cancellationToken = new CancellationTokenSource();
+
+            var task = new Task(() =>
+            {
+                int i = 0;
+                while (true)
+                {
+                    //if (cancellationToken.IsCancellationRequested)
+                    //    //break;
+                    //    throw new OperationCanceledException();
+                    cancellationToken.Token.ThrowIfCancellationRequested();//like break;
+                    Console.WriteLine($"{i++}\t");
+                }
+            }, cancellationToken.Token);
+            task.Start();
+            Console.ReadKey();
+            cancellationToken.Cancel();
+            Console.WriteLine("Task cancelled");
+        }
+    }
+}
